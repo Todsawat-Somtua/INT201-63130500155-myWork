2e23f0f0510c868b7be816de920bbc7a4b0966cd
Todsawat Somtua <todsawat.st@mail.kmutt.ac.th>
2021-12-08 11:52:55 +0700

    FINAL!!

---
+++ onClass/findZodiac.js
@@ -0,0 +1,43 @@
+// let year = 2000 ;  //assigned year hear !!
+let zodiac = ["Monkey", "Cock", "Dog", "Pig", "Rat", "Ox", "Tiger", "Rabbit", "Dragon", "Snake", "Horse", "Sheep"]
+
+/*
+
+// Simplest
+console.log(`Your zodiac is: ${zodiac[ year % zodiac.length ]}`);
+
+// Switch case
+switch (year % zodiac.length){
+    case 0: return console.log(`Hello, You are ${zodiac[0]}`) 
+        break;
+    case 1: return console.log(`Hello, You are ${zodiac[1]}`)
+        break;
+    case 2: return console.log(`Hello, You are ${zodiac[2]}`)
+        break;
+    case 3: return console.log(`Hello, You are ${zodiac[3]}`)
+        break;
+    case 4: return console.log(`Hello, You are ${zodiac[4]}`)
+        break;
+    case 5: return console.log(`Hello, You are ${zodiac[5]}`)
+        break;
+    case 6: return console.log(`Hello, You are ${zodiac[6]}`)
+        break;
+    case 7: return console.log(`Hello, You are ${zodiac[7]}`)
+        break;
+    case 8: return console.log(`YOO!! You have zodiac just like me !! It's ${zodiac[8]}`)
+        break;
+    case 9: return console.log(`Hello, You are ${zodiac[9]}`)
+        break;
+    case 10: return console.log(`Hello, You are ${zodiac[10]}`) 
+        break;
+    case 11: return console.log(`Hello, You are ${zodiac[11]}`)
+        break;
+}
+
+// function
+function zodiacCal(year) {
+    return zodiac[Number(year) % 12];
+};
+console.log(zodiacCal(2000));
+
+*/
